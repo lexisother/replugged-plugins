b181e45bcd736fc851d6818bcf9503aa760dc013
Alyxia Sother <alyxia@riseup.net>
2023-08-01 10:28:34 +0200

    [emojiBlacklist] Redo settings UI

--- plugins/emojiBlacklist/Settings.tsx
+++ plugins/emojiBlacklist/Settings.tsx
@@ -1,5 +1,5 @@
-import { common, components, settings, webpack } from "replugged";
-const { Text, TextInput, Flex } = components;
+import { common, components, settings } from "replugged";
+const { Text, TextInput, Flex, FormItem } = components;
 const { React } = common;
 
 interface Settings {
@@ -7,13 +7,6 @@ interface Settings {
 }
 
 export const pSettings = await settings.init<Settings>("dev.alyxia.emojiBlacklist");
-
-const Margins = webpack.getByProps([
-  "marginTop8",
-  "marginCenterHorz",
-  "marginLarge",
-  "marginBottom20",
-])!;
 
 export function Settings(): React.ReactElement {
   const emojiList = pSettings.get("emojiList", "");
@@ -33,18 +26,26 @@ export function Settings(): React.ReactElement {
 
   return (
     <>
-      <Text>
-        Enter your list of emojis to blacklist here. They must be comma separated values, like so:
-        `370877996112019456,1090374917058465792,967472493667450890`
-      </Text>
-      <Flex direction={Flex.Direction.HORIZONTAL} className={Margins.marginBottom20}>
-        <TextInput value={text} onChange={(text) => setText(text)} />
-        {saved && (
-          <Text tag={"h4"} selectable={false} variant="text-sm/normal">
+      <FormItem
+        title="Emoji blacklist"
+        note="Enter your list of emojis to blacklist here. They must be comma separated values, like so: `370877996112019456,1090374917058465792,967472493667450890`"
+        divider={true}>
+        <Flex align={Flex.Align.CENTER} direction={Flex.Direction.HORIZONTAL}>
+          {/* HACK: Wrapping the TextInput in a Flex.Child does nothing, and
+          setting the flex style directly on it doesn't help either because it is
+          wrapped in an `inputContainer` div. */}
+          <div style={{ flex: "1 0 auto", marginRight: "20px" }}>
+            <TextInput value={text} onChange={(text) => setText(text)} />
+          </div>
+          <Text
+            tag={"h4"}
+            selectable={false}
+            variant="text-sm/normal"
+            style={{ visibility: saved ? "visible" : "hidden" }}>
             Saved!
           </Text>
-        )}
-      </Flex>
+        </Flex>
+      </FormItem>
     </>
   );
 }
