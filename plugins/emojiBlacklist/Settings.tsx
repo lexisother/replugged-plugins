1cc877af0d87339501831a0b2392c6c2751d58be
Alyxia Sother <alyxia@riseup.net>
2023-08-01 22:33:24 +0200

    [emojiBlacklist] Rewrite the settings page & logic

--- plugins/emojiBlacklist/Settings.tsx
+++ plugins/emojiBlacklist/Settings.tsx
@@ -1,6 +1,11 @@
-import { common, components, settings } from "replugged";
-const { Text, TextInput, Flex, FormItem } = components;
+import { common, components, settings, webpack } from "replugged";
+const { Text, TextInput, Flex, FormItem, Button } = components;
 const { React } = common;
+
+const Margins = webpack.getByProps(
+  ["marginTop8", "marginCenterHorz", "marginLarge", "marginBottom20"],
+  { all: false, raw: false },
+)!;
 
 interface Settings {
   emojiList: string;
@@ -9,9 +14,12 @@ interface Settings {
 export const pSettings = await settings.init<Settings>("dev.alyxia.emojiBlacklist");
 
 export function Settings(): React.ReactElement {
+  // These two are never touched after initialization, use the list state for this!!!
   const emojiList = pSettings.get("emojiList", "");
+  const emojiObject = emojiList.split(",").map((e) => ({ text: e, id: Math.random() }));
+
+  let [list, setList] = React.useState(emojiObject);
   let [once, setOnce] = React.useState(false);
-  let [text, setText] = React.useState(emojiList);
   let [saved, setSaved] = React.useState(false);
 
   React.useEffect(() => {
@@ -19,10 +27,48 @@ export function Settings(): React.ReactElement {
       setOnce(true);
       return;
     }
-    pSettings.set("emojiList", text);
+    pSettings.set("emojiList", list.map((e) => e.text).join(","));
     setSaved(true);
     setTimeout(() => setSaved(false), 1500);
-  }, [text, setSaved]);
+  }, [list, setSaved]);
+
+  const inputFields = React.useMemo(
+    () =>
+      list.map((emoji, i) => (
+        <Flex
+          key={`${emoji.id}`}
+          direction={Flex.Direction.HORIZONTAL}
+          className={Margins.marginBottom20 as string}>
+          <Flex.Child shrink={0} basis="99%">
+            {/* HACK: Wrapping the TextInput in a Flex.Child does nothing, and
+              setting the flex style directly on it doesn't help either because it is
+              wrapped in an `inputContainer` div. */}
+            <div style={{ flex: "1 0 auto" }}>
+              <TextInput
+                value={list[i].text}
+                error={!/^\d{17,19}$/.test(emoji.text) ? "Invalid ID" : ""}
+                onChange={(text) => {
+                  setList(list.map((val) => (val.id === emoji.id ? { ...val, text } : val)));
+                }}
+              />
+            </div>
+          </Flex.Child>
+          <Flex.Child grow={0} basis="1%">
+            <Button
+              hover={Button.Hovers.RED}
+              look={Button.Looks.OUTLINED}
+              color={Button.Colors.LINK}
+              style={{ minWidth: "unset" }}
+              onClick={() => {
+                setList(list.filter((val) => emoji.id !== val.id));
+              }}>
+              {"\u00d7"}
+            </Button>
+          </Flex.Child>
+        </Flex>
+      )),
+    [list, setList],
+  );
 
   return (
     <>
@@ -30,20 +76,24 @@ export function Settings(): React.ReactElement {
         title="Emoji blacklist"
         note="Enter your list of emojis to blacklist here. They must be comma separated values, like so: `370877996112019456,1090374917058465792,967472493667450890`"
         divider={true}>
-        <Flex align={Flex.Align.CENTER} direction={Flex.Direction.HORIZONTAL}>
-          {/* HACK: Wrapping the TextInput in a Flex.Child does nothing, and
-          setting the flex style directly on it doesn't help either because it is
-          wrapped in an `inputContainer` div. */}
-          <div style={{ flex: "1 0 auto", marginRight: "20px" }}>
-            <TextInput value={text} onChange={(text) => setText(text)} />
-          </div>
-          <Text
-            tag={"h4"}
-            selectable={false}
-            variant="text-sm/normal"
-            style={{ visibility: saved ? "visible" : "hidden" }}>
-            Saved!
-          </Text>
+        <Text
+          tag={"h4"}
+          selectable={false}
+          variant="text-sm/normal"
+          style={{ visibility: saved ? "visible" : "hidden" }}>
+          Saved!
+        </Text>
+        <Flex direction={Flex.Direction.VERTICAL}>
+          {inputFields}
+
+          <Button
+            look={Button.Looks.FILLED}
+            color={Button.Colors.GREEN}
+            onClick={() => {
+              setList([...list, { id: Math.random(), text: "" }]);
+            }}>
+            Add new entry
+          </Button>
         </Flex>
       </FormItem>
     </>
